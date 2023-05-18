c1b4841ee1c44ae2124c6da10a4d066136afee16
Alexandr Demicev <alexandr.demicev@suse.com>
2023-05-18 18:17:49 +0200

    Image building process
    Signed-off-by: Alexandr Demicev <alexandr.demicev@suse.com>

--- image-builder/scripts/ubuntu/bootstrap.sh
+++ image-builder/scripts/ubuntu/bootstrap.sh
@@ -4,18 +4,39 @@ set -o nounset
 set -o pipefail
 
 echo "Install required packages"
+
 apt-get update && \
-apt-get install -y \
+DEBIAN_FRONTEND=noninteractive apt-get install -y \
         curl \
         openssh-server \
         cloud-init \
         systemd \
+        awscli \
+
+snap install amazon-ssm-agent --classic
+snap start amazon-ssm-agent
+
+cloud-init clean
+rm -f /var/log/cloud-init*
+
+
+mkdir -p /usr/lib/python3/dist-packages/cloudinit
+mv /tmp/feature_overrides.py /usr/lib/python3/dist-packages/cloudinit/feature_overrides.py
+
+systemctl enable cloud-final
+systemctl enable cloud-config
+systemctl enable cloud-init
+systemctl enable cloud-init-local
+systemctl start cloud-final
+systemctl start cloud-config
+systemctl start cloud-init
+systemctl start cloud-init-local
 
 echo "Install RKE2 components"
 mkdir -p /opt/rke2-artifacts
-curl -sfL -o /opt/rke2-artifacts/rke2-images.linux-amd64.tar.zst https://github.com/rancher/rke2/releases/download/v${3}/rke2-images.linux-amd64.tar.zst
-curl -sfL -o /opt/rke2-artifacts/rke2.linux-amd64.tar.gz https://github.com/rancher/rke2/releases/download/v${3}/rke2.linux-amd64.tar.gz
-curl -sfL -o /opt/rke2-artifacts/sha256sum-amd64.txt https://github.com/rancher/rke2/releases/download/v${3}/sha256sum-amd64.txt
+curl -sfL -o /opt/rke2-artifacts/rke2-images.linux-amd64.tar.zst https://github.com/rancher/rke2/releases/download/v${1}/rke2-images.linux-amd64.tar.zst
+curl -sfL -o /opt/rke2-artifacts/rke2.linux-amd64.tar.gz https://github.com/rancher/rke2/releases/download/v${1}/rke2.linux-amd64.tar.gz
+curl -sfL -o /opt/rke2-artifacts/sha256sum-amd64.txt https://github.com/rancher/rke2/releases/download/v${1}/sha256sum-amd64.txt
 curl -sfL -o /opt/install.sh https://get.rke2.io
 
 echo "Done"
